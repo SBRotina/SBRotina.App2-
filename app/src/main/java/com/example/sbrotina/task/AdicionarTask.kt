7a94e552216e8a5ae77e97d2ae708fb593383e91
SBRotina <sbrotinasaudavel006@gmail.com>
2023-05-30 18:08:26 -0300

    first commit

---
+++ app/src/main/java/com/example/sbrotina/task/AdicionarTask.kt
@@ -0,0 +1,12 @@
+package com.example.sbrotina.task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sbrotina.R
+
+class AdicionarTask : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adicionar_task)
+    }
+}
